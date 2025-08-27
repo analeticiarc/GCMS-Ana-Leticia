88ac493af64d33a1b8debf45f10f7edda3dd18dd
ANA LETICIA <analetrc@gmail.com>
2025-08-26 23:53:08 -0300

    quebrando teste novamente para parte 2

--- src/test/java/com/example/WebApp/WebAppApplicationTests.java
+++ src/test/java/com/example/WebApp/WebAppApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -64,7 +63,7 @@ class WebAppApplicationTests {
         this.mockMvc.perform(get("/AnaLeticia/hobby"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content()
-                        .string(containsString("Hello, I am Ana Leticia! My hobby is painting.")));
+                        .string(containsString("Hello, I am Ana Leticia! My hobby is painting")));
 
     }
 
